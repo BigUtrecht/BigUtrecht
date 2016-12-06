a5588cbef50941de3b2473d7ab6637f0c312ea14
GBPeters <gijstebrij@gmail.com>
2016-12-06 13:18:22 +0100

    Analysis template function added. Please use this as a basis for creating analysis functions.

---
+++ analysis/template.py
@@ -0,0 +1,27 @@
+from app.spark import *
+from parquet import parquet
+
+
+def dummyAnalysis():
+    """
+    Dummy analysis function
+    To be used as analysis template. Copy function body
+    Prints minimum and maximum date
+    :return: None
+    """
+    with Session() as s:
+        # s is the SparkSession. It is the entry point for spark operations.
+        # s.sparkContext contains the SparkContext, if you really need it.
+        telling = parquet.readTelling(s)  # DataFrame containing telling data
+
+        # Dummy code, prints earliest and latest date
+        print telling.agg({"Datum": "min"}).collect()
+        print telling.agg({"Datum": "max"}).collect()
+
+        # Dummy code, creates RDD from datum column and stores it in a result parquet table
+        r = telling.select("Datum").rdd
+        parquet.saveResults(s, r, "dummyresult")
+
+
+if __name__ == '__main__':
+    dummyAnalysis()
--- parquet/parquet.py
+++ parquet/parquet.py
@@ -1,22 +1,83 @@
 from pydoop.hdfs import hdfs
+from pyspark import RDD
+from pyspark.sql import DataFrame
 
 from app.spark import *
 
 TELLING = path.join(PARQUET_DIR, "telling")
+RESULT_DIR = path.join(PARQUET_DIR, "results")
 h = hdfs()
 if not h.exists(PARQUET_DIR):
     h.create_directory(PARQUET_DIR)
 
+if not h.exists(RESULT_DIR):
+    h.create_directory(RESULT_DIR)
+
 
 def saveTelling(dataframe):
+    """
+    Appends DataFrame to telling parquet table
+    :param DataFrame: the DataFrame to append
+    :return: None
+    """
     dataframe.write.parquet(TELLING, mode="append")
 
 
 def readTelling(session):
+    """
+    Returns a DataFrame containing the telling data stored as a parquet table
+    :param session: SparkSession to be used for this DataFrame
+    :return: the DataFrame
+    """
     return session.read.parquet(TELLING)
 
 
 def clearTelling():
+    """
+    Removes the telling parquet table, if it exists
+    :return: None
+    """
     h = hdfs()
     if h.exists(TELLING):
-        h.delete(path.join(TELLING))
+        h.delete(TELLING)
+
+
+def saveResults(session, data, name, mode="overwrite"):
+    """
+    Saves a DataFrame or RDD in a parquet table with given name. Overwrites by default
+    :param data: the data to be stored
+    :param name: the name of the result table
+    :param mode: the saveMode
+    :return: None
+    """
+    if isinstance(data, DataFrame):
+        dataframe = data
+    elif isinstance(data, RDD):
+        dataframe = session.createDataFrame(data)
+    else:
+        raise ValueError("Data is DataFrame nor RDD.")
+    p = path.join(RESULT_DIR, name)
+    dataframe.write.parquet(p, mode=mode)
+
+
+def readResults(session, name):
+    """
+    Reads a result parquet table with given name into a DataFrame
+    :param session: the SparkSession to use with this DataFrame
+    :param name: the target result parquet table name
+    :return: the DataFrame
+    """
+    p = path.join(RESULT_DIR, name)
+    return session.read.parquet(p)
+
+
+def clearResults(name=""):
+    """
+    Clears target result parquet table name, or all result parquet tables if no name is given
+    :param name: the target result parquet table name
+    :return: None
+    """
+    p = path.join(RESULT_DIR, name)
+    h = hdfs()
+    if h.exists(p):
+        h.delete(p)
