f470a24b0cfb6ca716299450ebbe8c985008a72e
JornHabes <jorn.habes@wur.nl>
2016-12-07 17:09:01 +0100

    Added rush hour averages calculation, and maps the rush hour averages

--- Sandbox/map.py
+++ Sandbox/map.py
@@ -1,40 +1,74 @@
 import folium
 import pandas as pd
 from osgeo.osr import SpatialReference, CoordinateTransformation
+from pyspark.sql.functions import *
 
-## Read the information about the location from a .csv file to a pandas dataframe
-locationdata = pd.read_csv('/home/WUR/holts009/Downloads/UTREC_F_2015_jun_Locatie.csv')
+def map_dataframe(dataframe, variable_string1, max_color, mid_color, popup_string1, save_string,
+                  variable_string2 = None, popup_string2 = None, variable_string3 = None, popup_string3 = None,
+                  variable_string4 = None, popup_string4 = None):
+        locationdata = pd.read_csv('/home/WUR/habes001/Downloads/UTREC_F_2015_jun_Locatie.csv')
 
-## Used to convert coordinates from RDnew to WGS84
-epsg28992 = SpatialReference()
-epsg28992.ImportFromEPSG(28992)
+        ## Used to convert coordinates from RDnew to WGS84
+        epsg28992 = SpatialReference()
+        epsg28992.ImportFromEPSG(28992)
 
-epsg28992.SetTOWGS84(565.237, 50.0087, 465.658, -0.406857, 0.350733, -1.87035, 4.0812)
+        epsg28992.SetTOWGS84(565.237, 50.0087, 465.658, -0.406857, 0.350733, -1.87035, 4.0812)
 
-epsg4326 = SpatialReference()
-epsg4326.ImportFromEPSG(4326)
+        epsg4326 = SpatialReference()
+        epsg4326.ImportFromEPSG(4326)
 
-rd2latlon = CoordinateTransformation(epsg28992, epsg4326)
-latlon2rd = CoordinateTransformation(epsg4326, epsg28992)
+        rd2latlon = CoordinateTransformation(epsg28992, epsg4326)
+        latlon2rd = CoordinateTransformation(epsg4326, epsg28992)
 
-## Set center of map to center of Utrecht and create map
-SF_COORDINATES = (52.092876, 5.104480)
-map = folium.Map(location=SF_COORDINATES, zoom_start=14)
+        ## Set center of map to center of Utrecht and create map
+        SF_COORDINATES = (52.092876, 5.104480)
+        map = folium.Map(location=SF_COORDINATES, zoom_start=14)
 
-## Set a marker for each location in the dataframe
-for each in locationdata.iterrows():
-    ## Select all coordinates which are not 'null'
-    if str(each[1]['XcoordinaatRD'])[0].isdigit():
-        ## Convert coordinates
-        X, Y, Z = rd2latlon.TransformPoint(each[1]['XcoordinaatRD'], each[1]['YcoordinaatRD'])
-        ## Create a marker for each location
-        folium.Marker(
-            location=[Y, X],
-            popup=(each[1]['MeetpuntRichtingCode']),
-            icon=folium.Icon(color='green', icon='road')).add_to(map)
+        ## Set a marker for each location in the dataframe
+        for each in locationdata.iterrows():
+            for line in dataframe.collect():
+                if each[1]['MeetpuntRichtingCode'] == line['UniekeMeetpuntRichtingCode']+'-1':
+                    if line[variable_string1] > max_color:
+                        color = 'red'
+                    elif line[variable_string1] > mid_color:
+                        color = 'orange'
+                    else:
+                        color = 'green'
+                    ## Select all coordinates which are not 'null'
+                    if str(each[1]['XcoordinaatRD'])[0].isdigit():
+                        ## Convert coordinates
+                        X, Y, Z = rd2latlon.TransformPoint(each[1]['XcoordinaatRD'], each[1]['YcoordinaatRD'])
+                        ## Create a marker for each location
+                        if variable_string4 != None:
+                            folium.Marker(
+                                location=[Y, X],
+                                popup=(folium.Popup(popup_string1+': '+str(int(line[variable_string1]))+', '+
+                                                    popup_string2+': '+str(int(line[variable_string2]))+', '+
+                                                    popup_string3+': '+str(int(line[variable_string3]))+', '+
+                                                    popup_string4+': '+str(int(line[variable_string4]))
+                                                    , max_width=200)),
+                                icon=folium.Icon(color=color, icon='road')).add_to(map)
+                        elif variable_string3 != None:
+                            folium.Marker(
+                                location=[Y, X],
+                                popup=(folium.Popup(popup_string1+': '+str(int(line[variable_string1]))+', '+
+                                                    popup_string2+': '+str(int(line[variable_string2]))+', '+
+                                                    popup_string3+': '+str(int(line[variable_string3]))
+                                                    , max_width=200)),
+                                icon=folium.Icon(color=color, icon='road')).add_to(map)
+                        elif variable_string2 != None:
+                            folium.Marker(
+                                location=[Y, X],
+                                popup=(folium.Popup(popup_string1+': '+str(int(line[variable_string1]))+', '+
+                                                    popup_string2+': '+str(int(line[variable_string2])), max_width=200)),
+                                icon=folium.Icon(color=color, icon='road')).add_to(map)
+                        else:
+                            folium.Marker(
+                                location=[Y, X],
+                                popup=(popup_string1 + ': ' + str(int(line[variable_string1]))),
+                                icon=folium.Icon(color=color, icon='road')).add_to(map)
 
-## Save the map to a .html file
-map.save('/tmp/map.html')
-
+        ## Save the map to a .html file
+        map.save(save_string)
 ## Display in Jupyter Notebook:
 # map
--- analysis/template.py
+++ analysis/template.py
@@ -1,6 +1,12 @@
 from app.spark import *
 from parquet import parquet
+from pyspark.sql.functions import *
+from Sandbox import map
+import datetime
 
+import folium
+import pandas as pd
+from osgeo.osr import SpatialReference, CoordinateTransformation
 
 def dummyAnalysis():
     """
@@ -14,13 +20,77 @@ def dummyAnalysis():
         # s.sparkContext contains the SparkContext, if you really need it.
         telling = parquet.readTelling(s)  # DataFrame containing telling data
 
-        # Dummy code, prints earliest and latest date
-        print telling.agg({"Datum": "min"}).collect()
-        print telling.agg({"Datum": "max"}).collect()
+        # Load rush hour data
+        #rush_time =
+        #select_time = lambda t:
+
+        # Calculate moments with highest intensity
+        #intensity = telling.orderBy(telling.Intensiteit.desc()).show(40)
+        #point_average = telling.groupBy("UniekeMeetpuntRichtingCode").avg("Intensiteit").orderBy("avg(Intensiteit)", ascending = False).show(34)
+        #telling.show(5)
+
+        ## Dataframe with each vehicle type added together
+        add_category = telling.groupBy(["UniekeMeetpuntRichtingCode", "Timestamp", "Tijd"]).agg({"Intensiteit": "sum"}).orderBy("Timestamp", ascending = True)
+        # add_category.show(50)
+
+        # filter_zero = add_category.filter(add_category["sum(Intensiteit)"] > 0)
+        # #filter_zero.show(5)
+        # point_max = filter_zero.groupBy("UniekeMeetpuntRichtingCode").agg({"sum(Intensiteit)": "max"}).orderBy("max(sum(Intensiteit))", ascending=False)
+        # #point_max.show(5)
+        #
+        # ## Dataframe with max intensity per point
+        # max_total = filter_zero.join(point_max, (point_max["UniekeMeetpuntRichtingCode"] == filter_zero["UniekeMeetpuntRichtingCode"]) & (point_max["max(sum(Intensiteit))"] == filter_zero["sum(Intensiteit)"]),"inner")\
+        #     .drop(filter_zero["UniekeMeetpuntRichtingCode"]).drop(point_max["max(sum(Intensiteit))"]).withColumnRenamed("sum(Intensiteit)", "MaxIntensiteit")
+        # #max_total.show()
+
+        add_category.registerTempTable("add_category")
+        filter_rush_morning = s.sql(
+            "SELECT * FROM add_category WHERE Tijd IN ('08:00', '08:15', '08:30', '08:45', '09:00', '09:15', '09:30', '09:45', '10:00')")
+
+        point_rush_morning_avg = filter_rush_morning.groupBy("UniekeMeetpuntRichtingCode").agg(
+            {"sum(Intensiteit)": "avg"}).withColumnRenamed('avg(sum(Intensiteit))', 'MorningIntensity')
+        # point_rush_morning_avg.show(30)
+
+        filter_rush_evening = s.sql(
+            "SELECT * FROM add_category WHERE Tijd IN ('17:00', '17:15', '17:30', '17:45', '18:00', '18:15', '18:30', '18:45', '19:00')")
+
+        point_rush_evening_avg = filter_rush_evening.groupBy("UniekeMeetpuntRichtingCode").agg(
+            {"sum(Intensiteit)": "avg"}).withColumnRenamed('avg(sum(Intensiteit))', 'EveningIntensity')
+        # point_rush_evening_avg.show(30)
+
+        point_rush_avg = point_rush_morning_avg.join(point_rush_evening_avg, (point_rush_morning_avg.UniekeMeetpuntRichtingCode == point_rush_evening_avg.UniekeMeetpuntRichtingCode)).\
+            drop(point_rush_evening_avg.UniekeMeetpuntRichtingCode)
+        # point_rush_avg.show(34)
+
+        point_rush_avg_in = point_rush_avg.filter(point_rush_avg.UniekeMeetpuntRichtingCode.endswith('1')).\
+            withColumn('UniekeMeetpuntRichtingCode', regexp_replace('UniekeMeetpuntRichtingCode', '-1', '')).\
+            withColumnRenamed('MorningIntensity', 'MorningIntensityIn').\
+            withColumnRenamed('EveningIntensity', 'EveningIntensityIn')
+        point_rush_avg_out = point_rush_avg.filter(point_rush_avg.UniekeMeetpuntRichtingCode.endswith('2')).\
+            withColumn('UniekeMeetpuntRichtingCode', regexp_replace('UniekeMeetpuntRichtingCode', '-2', '')).\
+            withColumnRenamed('MorningIntensity', 'MorningIntensityOut').\
+            withColumnRenamed('EveningIntensity', 'EveningIntensityOut')
+
+
+        point_rush_avg_in_out = point_rush_avg_in.join(point_rush_avg_out, (point_rush_avg_in.UniekeMeetpuntRichtingCode == point_rush_avg_out.UniekeMeetpuntRichtingCode)).\
+            drop(point_rush_avg_out.UniekeMeetpuntRichtingCode)
+
+        map.map_dataframe(point_rush_avg_in_out, 'MorningIntensityIn', 1000, 500, 'Morning average intensity in',
+                          '/tmp/map3.html', 'EveningIntensityIn', 'Evening average intensity in', 'MorningIntensityOut',
+                          'Morning average intensity out', 'EveningIntensityOut', 'Evening average intensity out')
+
+        # filter_rush = add_category.filter(add_category.Tijd == '08:00' | add_category.Tijd == '08:15')
+        # filter_rush.show(50)
+        #
+        #
+        #
+        # map.map_dataframe(max_total, 'MaxIntensiteit', 1000, 500, 'Maximum intensity', '/tmp/map1.html')
+        #
+        # map.map_dataframe()
 
         # Dummy code, creates RDD from datum column and stores it in a result parquet table
-        r = telling.select("Datum").rdd
-        parquet.saveResults(s, r, "dummyresult")
+        #r = telling.select("Datum").rdd
+        #parquet.saveResults(s, r, "dummyresult")
 
 
 if __name__ == '__main__':
