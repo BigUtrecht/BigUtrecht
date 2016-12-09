1431fdfdd8458335cdaeba7bc33f40c635221c40
GBPeters <gijstebrij@gmail.com>
2016-12-09 11:49:34 +0100

    App moved up

---
+++ app.py
@@ -0,0 +1,67 @@
+import tkMessageBox
+import webbrowser
+from Tkinter import *
+
+from analysis import maxintensity
+from analysis.averagerush import avgRushAnalysis
+from constants.spark import Session
+from etl.extractcsv import etl
+
+
+class GUI(Frame):
+    def __init__(self, master=None):
+        with Session() as spark:
+            Frame.__init__(self, master)
+            self.master.title("BigUtrecht Tiny GUI")
+
+            self.buttons = []
+
+            self.ETLButton = Button(self, text="Download and Extract")
+            self.ETLButton["command"] = self.runETL
+            self.ETLButton.grid(row=0, column=0)
+
+            self.buttons.append(self.ETLButton)
+
+            self.map1Button = Button(self, text="Show Maximum Intensity Map")
+            self.map1Button["command"] = self.showMap1
+            self.map1Button.grid(row=1, column=0)
+
+            self.map2Button = Button(self, text="Show Average Rush Hour Intensity Map")
+            self.map2Button["command"] = self.showMap2
+            self.map2Button.grid(row=2, column=0)
+
+            self.buttons.append(self.map1Button)
+            self.buttons.append(self.map2Button)
+
+            self.grid()
+
+    def enableAll(self):
+        for button in self.buttons:
+            button["state"] = "normal"
+
+    def disableAll(self):
+        for button in self.buttons:
+            button["state"] = "disabled"
+
+    def runETL(self):
+        self.disableAll()
+        etl()
+        tkMessageBox.showinfo("ETL Succeeded!")
+        self.enableAll()
+
+    def showMap1(self):
+        self.disableAll()
+        maxintensity.maxPointAnalysis()
+        webbrowser.open("/tmp/map_max_intensity.html")
+        self.enableAll()
+
+    def showMap2(self):
+        self.disableAll()
+        avgRushAnalysis()
+        webbrowser.open("/tmp/map_avg_rush.html")
+        self.enableAll()
+
+
+if __name__ == "__main__":
+    gui = GUI()
+    gui.mainloop()
--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 # Welcome!
 echo "Downloading BigUtrecht and dependencies! Thanks for watching, Ioannis!"
-cd ~
 
 # Set Spark environment
 export SPARK_HOME=/mnt/spark-2.0.0-bin-hadoop2.7
 export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.10.1-src
 
+# Download and install PROJ
+cd ~
+wget http://download.osgeo.org/proj/proj-4.9.3.tar.gz
+tar xvfz proj-4.9.3.tar.gz
+cd proj-4.9.3
+mkdir build
+./configure --prefix=/usr/
+make
+make install
+
 # Download and install GDAL
+cd ~
 wget "http://download.osgeo.org/gdal/2.1.0/gdal-2.1.0.tar.gz"
 tar -zxvf gdal-2.1.0.tar.gz
 cd gdal-2.1.0
@@ -26,10 +36,11 @@ sudo -E pip install Folium
 
 # Clone and run BigUtrecht
 cd ~
+rm -R -f BigUtrecht
 mkdir BigUtrecht
 cd BigUtrecht
 git clone https://github.com/BigUtrecht/BigUtrecht.git
-python BigUtrecht/analysis/flows.py
+python BigUtrecht/app/app.py
 
 
 
